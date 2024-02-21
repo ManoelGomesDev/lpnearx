d112fb91f717091f6d62a5b24bc1c256020866fe
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2024-02-20 22:25:33 -0300

    test video 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-tr from-bgDark to-bgLight ">
       <Header />
-      <div className="flex justify-center h-screen items-center " id="section-video">
+      <div className="flex justify-center h-screen " id="section-video">
         <iframe
           id="panda-224758e4-4a95-4622-ab07-153ef8583e62"
           src="https://player-vz-350c0f87-16f.tv.pandavideo.com.br/embed/?v=224758e4-4a95-4622-ab07-153ef8583e62"
           style={{ border: "none" }}
           allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
           allowFullScreen={true}
-          className="h-[140px] w-[280px] tablet:h-[360px] tablet:w-[720px] macair:w-[1240px] macair:h-[720px] desktop:h-[720px] desktop:w-[1440px] rounded-md "
+          className="h-[720px] w-[300px] tablet:h-[360px] tablet:w-[720px] macair:w-[1240px] macair:h-[720px] desktop:h-[720px] desktop:w-[1440px] rounded-md "
           data-fetchpriority="high"
         ></iframe>
       </div>
