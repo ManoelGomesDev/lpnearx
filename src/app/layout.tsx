08b83dc56e5014af57300d1aab0890fe52f8b197
Adilson Oliveira <adilsonoj@yahoo.com.br>
2024-03-04 15:02:31 -0300

    Add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,24 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" type='image/x-icon' />
-        
+        <script type='text/javascript' async dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K32PS576');
+          `
+        }}></script>
+
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K32PS576"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}></noscript>
+        {children}
+      </body>
     </html>
   )
 }
